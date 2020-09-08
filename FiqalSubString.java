8f96b91fab6c920261ae18afd1f2af8a6ef377ee
Fiqallha <70952732+Fiqallha@users.noreply.github.com>
2020-09-08 18:56:57 +0700

    Add files via upload

---
+++ FiqalSubString.java
@@ -0,0 +1,14 @@
+package UTS;
+
+public class FiqalSubString {
+    public static void main(String[] args) {
+        String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+
+        System.out.println(alfabet);
+        System.out.println("" + alfabet.substring(5,6));
+        System.out.println("" + alfabet.substring(8,9));
+        System.out.println("" + alfabet.substring(16,17));
+        System.out.println("" + alfabet.substring(0,1));
+        System.out.println("" + alfabet.substring(11,12));
+    }
+}
